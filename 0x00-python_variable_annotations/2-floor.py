81dd44b55d964628e029e4e88ef41691116762aa
AniKanayo <micanian4real@gmail.com>
2023-07-07 01:55:35 +0000

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a float.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        int: The floor value of the input.
+
+    Example:
+        >>> floor(3.14)
+        3
+    """
+    return int(n)
+
+
+print(floor(3.14))  # Output: 3
