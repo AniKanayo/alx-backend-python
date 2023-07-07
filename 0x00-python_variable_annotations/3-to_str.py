3d8bd53eab638e36876aa6c56cf38f2cd83b6186
AniKanayo <micanian4real@gmail.com>
2023-07-07 02:30:14 +0000

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to its string representation.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
+
+
+if __name__ == "__main__":
+    result = to_str(3.14)
+    print(result == '3.14')
+    print({'n': float, 'return': str})
