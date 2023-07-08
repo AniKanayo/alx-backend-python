d5c70cd0e68f5dbc55683a9fe33728ed7a8b0328
AniKanayo <micanian4real@gmail.com>
2023-07-08 23:38:20 +0000

    4-define_variables

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-
+"""
+Module: 4-define_variables
+A module that exports 4-define_variables.a, 4-define_variables.pi,
+4-define_variables.i_understand_annotations, 4-define_variables.school
+"""
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
