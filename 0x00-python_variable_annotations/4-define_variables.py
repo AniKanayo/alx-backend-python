5b9b06df311be49a75eb273abdadeb3d1126a37d
AniKanayo <micanian4real@gmail.com>
2023-07-08 23:53:03 +0000

    4-define_variables

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -8,9 +8,3 @@ a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
 school: str = "Holberton"
-
-print(f"a is a {type(a).__name__} with a value of {a}")
-print(f"pi is a {type(pi).__name__} with a value of {pi}")
-print(f"i_understand_annotation is a {type(i_understand_annotations).__name__}"
-      f"with a value of {i_understand_annotations}")
-print(f"school is a {type(school).__name__} with a value of {school}")
