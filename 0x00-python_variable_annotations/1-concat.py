a51b05e3fd58199cafd9f86c1c50b69793dd4aca
AniKanayo <micanian4real@gmail.com>
2023-07-08 22:36:54 +0000

    1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-"""Write a type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string """
+"""Here, we are required to write a type-annotated function concat 
+   that takes a string str1 and a string str2 as arguments and returns a 
+   concatenated string. This module would be imported by a
+   function that concatenates two strings """
 
 
 def concat(str1: str, str2: str) -> str:
