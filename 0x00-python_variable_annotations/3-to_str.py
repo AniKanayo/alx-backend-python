0693029436f50127f7fd60fad9bd831c627744e2
AniKanayo <micanian4real@gmail.com>
2023-07-08 23:00:55 +0000

    3-to_str.py

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+Module: 3-to_str
+This module export a function to_str that takes a float n as
+argument and returns the string representation of the float.
+"""
 
 
 def to_str(n: float) -> str:
