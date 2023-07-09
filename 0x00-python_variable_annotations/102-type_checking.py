040b6473eaefe9ad0a5d968183b3a9cde8b8617f
AniKanayo <micanian4real@gmail.com>
2023-07-09 01:40:58 +0000

    102-type_checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -32,8 +32,8 @@ def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
     return zoomed_in
 
 
-# array: Tuple[int, int, int] = (12, 72, 91)
+array: Tuple[int, int, int] = (12, 72, 91)
 
-# zoom_2x: List[int] = zoom_array(array)
+zoom_2x: List[int] = zoom_array(array)
 
-# zoom_3x: List[int] = zoom_array(array, 3)
+zoom_3x: List[int] = zoom_array(array, 3)
