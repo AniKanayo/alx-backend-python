e0daa8c1eebb9109a53e6fac05e35d4757fa6e9a
AniKanayo <micanian4real@gmail.com>
2023-07-10 22:05:17 +0000

    0x01. Python - Async

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+This module demonstrates the wait_n async routine.
+"""
+
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Async routine that spawns wait_random n times with the specified max_delay.
+    Returns a list of delays (float values) in ascending order.
+    """
+    tasks = []
+    for _ in range(n):
+        task = asyncio.create_task(wait_random(max_delay))
+        tasks.append(task)
+    delays = await asyncio.gather(*tasks)
+    delays.sort()  # Sorting the delays in ascending order
+    return delays
+
+# async def main():
+#    """
+#    Main function to demonstrate the wait_n async routine.
+#    """
+#    delays = await wait_n(10, 5)
+#    print(delays)
+#
+# asyncio.run(main())
