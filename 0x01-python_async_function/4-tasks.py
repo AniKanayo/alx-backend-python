b76cc2e80b266c3f1e39cb5635813f6052880d74
AniKanayo <micanian4real@gmail.com>
2023-07-10 23:34:53 +0000

    0x01. Python - Async

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,26 +5,14 @@ This module demonstrates the task_wait_random function.
 """
 
 import asyncio
-from typing import Task
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Task:
+def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Returns an asyncio.Task object for the wait_random coroutine with the specified max_delay.
     """
-    task = asyncio.create_task(wait_random(max_delay))
-    return task
-
-
-def main():
-    """
-    Main function to demonstrate the task_wait_random function.
-    """
-    max_delay = 5
-    tasks = [task_wait_random(max_delay) for _ in range(5)]
-    delays = asyncio.run(asyncio.gather(*tasks))
-    print(delays)
-
-if __name__ == "__main__":
-    main()
+    coroutines = [asyncio.create_task(task_wait_random(max_delay)) for _ in range(n)]
+    delays = await asyncio.gather(*coroutines)
+    return sorted(delays)
