418fa17f8aaf5cc12b66b91cc7b72b1d55bc2b67
AniKanayo <micanian4real@gmail.com>
2023-07-11 19:09:04 +0000

    Python - Async Comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 """
-Module
+Module: 0-async_generator
+This module provides an async generator coroutine for generating
+random floating-point numbers.
 """
 import asyncio
 import random
@@ -9,7 +11,13 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    An asynchronous generator that yields random floating-point numbers
+    between 0 and 10.
+    Returns:
+        AsyncGenerator[float, None]:An async generator that produces random
+        floating-point numbers.
+    """
     for _ in range(10):
-        """ Function """
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
