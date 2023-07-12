28b0c5eb3f2f976bfe967bc2715b27e2cd727cea
AniKanayo <micanian4real@gmail.com>
2023-07-12 00:48:07 +0000

    Python-Async

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -3,14 +3,17 @@
 """
 This module demonstrates the task_wait_random function.
 """
-
 import asyncio
-from typing import Task
-wait_random = __import__('3-tasks').wait_random
+from asyncio.tasks import Task
+from random import uniform
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 def task_wait_random(max_delay: int) -> Task:
     """
-    Returns an asyncio.Task object for the wait_random coroutine with the specified max_delay.
+    Returns an asyncio.Task object for the wait_random coroutine with the
+    specified max_delay.
 
     Args:
         max_delay: The maximum delay value.
@@ -18,17 +21,16 @@ def task_wait_random(max_delay: int) -> Task:
     Returns:
         An asyncio.Task object representing the wait_random coroutine task.
     """
-    loop = asyncio.get_event_loop()
-    task = loop.create_task(wait_random(max_delay))
-    return task
+    def wait_random():
+        async def inner_wait_random():
+            await asyncio.sleep(max_delay)
+            return
 
-def main():
-    """
-    Main function to demonstrate the task_wait_random function.
-    """
-    max_delay = 5
-    task = task_wait_random(max_delay)
-    print(type(task))
+        loop = asyncio.get_event_loop()
+        task = loop.create_task(inner_wait_random())
+        return task
 
-if __name__ == "__main__":
-    main()
+    return wait_random()
+#    loop = asyncio.get_event_loop()
+#    task = loop.create_task(wait_random(max_delay))
+#    return task
