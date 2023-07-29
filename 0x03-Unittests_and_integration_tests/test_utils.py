b3bc006e43914753761336ce54362b888e259eb2
AniKanayo <micanian4real@gmail.com>
2023-07-29 23:36:23 +0000

    Unittests and Integration Tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
+"""Module for testing the function access_nested_map"""
 import unittest
 from parameterized import parameterized
 
 
-def access_nested_map(nested_map: dict, path: tuple):
+def access_nested_map(nested_map: dict, path: tuple) -> object:
+    """Accesses a value in a nested map.
+
+    Args:
+        nested_map (dict): The nested map.
+        path (tuple): The path to the value in the map.
+
+    Returns:
+        object: The value that is at the given path in the map.
+    """
     # Assuming this function is already implemented
     pass
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """Class for testing the function access_nested_map"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """Tests the function access_nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
 
