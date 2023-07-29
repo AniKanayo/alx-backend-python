a8688e2094d87bcdf8e2d241430b9eee8f7f9d27
AniKanayo <micanian4real@gmail.com>
2023-07-29 23:33:17 +0000

    Unittests and Integration Tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,9 +2,11 @@
 import unittest
 from parameterized import parameterized
 
+
 def access_nested_map(nested_map: dict, path: tuple):
     # Assuming this function is already implemented
     pass
+
 
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
@@ -14,6 +16,7 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
 
 if __name__ == '__main__':
     unittest.main()
