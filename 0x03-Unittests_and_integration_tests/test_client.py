b7df88b916604055c63404287ab85e9118914481
AniKanayo <micanian4real@gmail.com>
2023-07-31 22:39:04 +0000

    test_utils.py

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+This module contains TestGithubOrgClient class for testing
+the GithubOrgClient class in the client module.
+"""
+
+import unittest
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+import client
+from typing import Any
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Class for testing GithubOrgClient class."""
+
+    @parameterized.expand([
+        ("google"),
+        ("abc")
+    ])
+    @patch('client.get_json', return_value={"payload": True})
+    def test_org(self, test_org: str, mock_get_json: Mock) -> None:
+        """Test method for GithubOrgClient.org method"""
+
+        # Initialize GithubOrgClient with test_org
+        github_org_client = client.GithubOrgClient(test_org)
+
+        # Test that GithubOrgClient.org returns the correct value
+        self.assertEqual(github_org_client.org, {"payload": True})
+
+        # Test that get_json was called exactly once with the correct argument
+        mock_get_json.assert_called_once_with
+        (f"https://api.github.com/orgs/{test_org}")
+
+
+if __name__ == '__main__':
+    unittest.main()
