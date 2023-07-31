6aa50099b083ab447bfc83c23ad8381c725eb156
AniKanayo <micanian4real@gmail.com>
2023-07-31 20:36:32 +0000

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python3
 """
-This module contains TestAccessNestedMap class and the function to be tested
+This module contains TestAccessNestedMap class
 """
 
 import unittest
 from parameterized import parameterized
+import utils
 from typing import Any, Dict, Tuple
 
 
-def access_nested_map(nested_map: Dict[Any, Any], path: Tuple[Any]) -> Any:
-    """Access nested map function"""
-    for key in path:
-        if key not in nested_map:
-            raise KeyError(key)
-        nested_map = nested_map[key]
-    return nested_map
-
-
 class TestAccessNestedMap(unittest.TestCase):
-    """Class for testing access_nested_map function"""
+    """Class for testing utils.access_nested_map function"""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -28,9 +20,9 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map: Dict[Any, Any],
                                path: Tuple[Any], expected: Any) -> None:
         """
-        Test method for access_nested_map function as required
+        Test method for utils.access_nested_map function
         """
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+        self.assertEqual(utils.access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
         ({}, ("a",)),
@@ -38,9 +30,9 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map_exception(self, nested_map: Dict[Any, Any],
                                          path: Tuple[Any]) -> None:
-        """Test method for access_nested_map function exception"""
+        """Test method for utils.access_nested_map function exception"""
         with self.assertRaises(KeyError) as cm:
-            access_nested_map(nested_map, path)
+            utils.access_nested_map(nested_map, path)
         self.assertEqual(cm.exception.args[0], path[-1])
 
 
