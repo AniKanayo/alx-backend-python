ca64ac9ed5fb903d9c1f38cb95eeaa4f7a0867f2
AniKanayo <micanian4real@gmail.com>
2023-07-31 20:05:43 +0000

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,34 +1,27 @@
 #!/usr/bin/env python3
-"""Module for testing the function access_nested_map"""
+"""
+This module contains TestAccessNestedMap class
+"""
+
 import unittest
 from parameterized import parameterized
-
-
-def access_nested_map(nested_map: dict, path: tuple) -> object:
-    """Accesses a value in a nested map.
-
-    Args:
-        nested_map (dict): The nested map.
-        path (tuple): The path to the value in the map.
-
-    Returns:
-        object: The value that is at the given path in the map.
-    """
-    # Assuming this function is already implemented
-    pass
-
+import utils
+from typing import Any, Dict, Tuple
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Class for testing the function access_nested_map"""
+    """Class for testing utils.access_nested_map function"""
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
-        """Tests the function access_nested_map"""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
-
+    def test_access_nested_map(self, nested_map: Dict[Any, Any], \
+                               path: Tuple[Any], expected: Any) -> None:
+        """
+        Test method for utils.access_nested_map function
+        """
+        self.assertEqual(utils.access_nested_map(nested_map, path), expected)
 
 if __name__ == '__main__':
     unittest.main()
